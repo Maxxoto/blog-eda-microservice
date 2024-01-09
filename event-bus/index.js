1a24eee567831e456ccd32e77115aa34b186cd1b
Ahmat Dani Setiawan <ahmatdanis24@gmail.com>
2024-01-10 01:42:20 +0700

    feat: add skaffold and fix for fe and be

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,36 +1,40 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const axios = require("axios");
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 
 const events = [];
 
-app.post("/events", (req, res) => {
+app.post('/events', (req, res) => {
   const event = req.body;
 
   events.push(event);
 
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  axios.post('http://posts-cluster-service:4000/events', event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
+  axios
+    .post('http://comments-cluster-service:4001/events', event)
+    .catch((err) => {
+      console.log(err.message);
+    });
+  axios.post('http://query-cluster-service:4002/events', event).catch((err) => {
     console.log(err.message);
   });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://localhost:4003/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  res.send({ status: "OK" });
+  axios
+    .post('http://moderation-cluster-service:4003/events', event)
+    .catch((err) => {
+      console.log(err.message);
+    });
+  res.send({ status: 'OK' });
 });
 
-app.get("/events", (req, res) => {
+app.get('/events', (req, res) => {
   res.send(events);
 });
 
 app.listen(4005, () => {
-  console.log("Listening on 4005");
+  console.log('Listening on 4005');
 });
